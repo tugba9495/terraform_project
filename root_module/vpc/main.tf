53aa346723daa206b6ffc999cd2aac42cae3dc5e
tugba9495 <tuba_7655@icloud.com>
2023-07-18 16:42:28 -0500

    added vpc components

--- root_module/vpc/main.tf
+++ root_module/vpc/main.tf
@@ -1,4 +1,13 @@
 module "aws_vpc" {
-source = "../../child_module/vpc"
-vpc_cidr = "10.0.0.0/16"
+source                    = "../../child_module/vpc"
+vpc_cidr                  = "10.0.0.0/16"
+public_subnet_cidr_1      =  "10.0.1.0/24"
+public_subnet_cidr_2      = "10.0.2.0/24"
+public_subnet_cidr_3      = "10.0.3.0/24"
+private_subnet_cidr_1     = "10.0.11.0/24"
+private_subnet_cidr_2     = "10.0.12.0/24"
+private_subnet_cidr_3     = "10.0.13.0/24"
+route_cidr                = "0.0.0.0/0"
+
+
 } 
