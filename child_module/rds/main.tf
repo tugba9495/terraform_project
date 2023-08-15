efac629664f83bd0b29eaee1a5b1f5b0125292d3
tugba9495 <tuba_7655@icloud.com>
2023-08-15 02:45:25 -0500

    modified rds jenkinsfile

--- child_module/rds/main.tf
+++ child_module/rds/main.tf
@@ -10,13 +10,25 @@ resource "aws_db_instance" "database" {
     password = var.password
     db_subnet_group_name = var.db_subnet_group_name
     # subnet_ids = var.subnet_ids
-    vpc_security_group_ids = var.vpc_security_group_ids
+    vpc_security_group_ids = [aws_security_group.database-sg.id]  
     publicly_accessible    = false
     skip_final_snapshot    = true
     iam_database_authentication_enabled = true
+    
     tags = merge(local.common_tags,{
         Name = replace(local.name,"rtype","terraform_db_instance_mysql")
     })
+}
 
+resource "aws_security_group" "database-sg" {
+    name = "allow_ingress_3306_database_sg"
+    description = "allow ingress 3306"
+    
+    # ... (other attributes)
+}
 
+resource "aws_security_group_rule" "database_3306_open" {
+    # ... (other attributes)
+
+    security_group_id = aws_security_group.database-sg.id  # Corrected here
 }
--- child_module/rds/variables.tf
+++ child_module/rds/variables.tf
@@ -28,10 +28,7 @@ variable "db_subnet_group_name" {
     type = string
   
 }
-variable "vpc_security_group_ids" {
-  type = list(string)
-  
-}
+
 variable "identifier" {
   
   type = string
@@ -51,5 +48,23 @@ variable "port" {
 variable "env" {
     type = string
     default = "rds"
+  
+}
+variable "aws_security_group_rule_from_3306" {
+  type = number
+  description = "port for ingress rule"
+  default = 3306
+  
+}
+variable "aws_security_group_rule_to_3306" {
+  type = number
+  description = "port for ingress rule"
+  default = 3306
+  
+}
+variable "cidr_blocks_for_ingress_security_group_3306" {
+  type = list(string)
+  description = "security group cidr block"
+  default = [ "98.253.243.198/32" ]
   
 }
