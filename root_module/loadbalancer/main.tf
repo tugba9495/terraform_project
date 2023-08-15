e4bb299cea66af8ccddab00decaaf1f55d7fa515
tugba9495 <tuba_7655@icloud.com>
2023-08-15 15:24:05 -0500

    modified rds jenkinsfile

--- root_module/loadbalancer/main.tf
+++ root_module/loadbalancer/main.tf
@@ -1,6 +1,6 @@
 module "aws_lb_target_group" {
 source = "../../child_module/loadbalancer"
-target_group_name = "load_balancer_target_group"
+target_group_name = "loadbalancertargetgroup"
 target_group_port = 80
 target_group_protocol = "HTTP"
 target_group_vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
