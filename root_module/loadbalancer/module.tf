a1ad6b771d2ceb2b1f56c2a365301543976480ea
tugba9495 <tuba_7655@icloud.com>
2023-08-15 15:15:50 -0500

    modified rds jenkinsfile

--- root_module/loadbalancer/module.tf
+++ root_module/loadbalancer/module.tf
@@ -1,14 +1,7 @@
-module "aws_vpc" {
-source                    = "../../child_module/vpc"
-vpc_cidr                  = "10.0.0.0/16"
-public_subnet_cidr_1      =  "10.0.1.0/24"
-public_subnet_cidr_2      = "10.0.2.0/24"
-public_subnet_cidr_3      = "10.0.3.0/24"
-private_subnet_cidr_1     = "10.0.11.0/24"
-private_subnet_cidr_2     = "10.0.12.0/24"
-private_subnet_cidr_3     = "10.0.13.0/24"
-route_cidr                = "0.0.0.0/0"
-route_for_private_route   = "0.0.0.0/0"
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
 
-
-} 
+  config = {
+    path = "../../root_module/vpc/terraform.tfstate"
+  }
+}
