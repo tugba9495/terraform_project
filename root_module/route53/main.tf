1f1e9425084fc6e9751276c719b2eb70ace66850
tugba9495 <tuba_7655@icloud.com>
2023-08-16 21:31:08 -0500

    modified  loadbalancer

--- root_module/route53/main.tf
+++ root_module/route53/main.tf
@@ -4,7 +4,7 @@ data "aws_route53_zone" "selected" {
  }
 module "aws_route53_record" {
     source =   "../../child_module/route53"
-    aws_route53_zone_id =  = data.aws_route53_zone.selected.zone_id
+    aws_route53_zone_id =  data.aws_route53_zone.selected.zone_id
     wordpress_a_rescord_name = "wordpress.tugba.link"
     aws_route53_record_type = "A"
     aws_route53_record_ttl = "300"
