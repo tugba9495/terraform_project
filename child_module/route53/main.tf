5536d2295da6f3090c2611d3b509460db3f6effa
tugba9495 <tuba_7655@icloud.com>
2023-08-16 21:28:35 -0500

    modified  loadbalancer

--- child_module/route53/main.tf
+++ child_module/route53/main.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "wordpress_a_record" {
   name    = var.wordpress_a_rescord
   type    = var.aws_route53_record_type
   ttl     = var.aws_route53_record_ttl
-  records = [aws_instance.jenkins_instance.public_ip]
+  
   alias {
     name = "wordpress-load-balancer-1132891715.us-west-1.elb.amazonaws.com"
     zone_id = "Z368ELLRRE2KJ0"
