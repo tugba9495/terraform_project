d2ed3799dbfb431da0e86953b468f598687c8151
tugba9495 <tuba_7655@icloud.com>
2023-08-16 21:27:35 -0500

    modified  loadbalancer

--- child_module/route53/locals.tf
+++ child_module/route53/locals.tf
@@ -1,14 +1,14 @@
-locals {
-  region = "us-west-1"
-  stage = "non-prod"
-  project = "wordpress"
-  name = "terraform-${local.stage}-${local.region}-${local.project}-rtype-${var.env}"
+# locals {
+#   region = "us-west-1"
+#   stage = "non-prod"
+#   project = "wordpress"
+#   name = "terraform-${local.stage}-${local.region}-${local.project}-rtype-${var.env}"
 
-  common_tags = {
-    env = var.env
-    project = local.project
-    team = "cloud"
-    owner = "tugba"
-    managed_by = "terraform"
-  }
-}
+#   common_tags = {
+#     env = var.env
+#     project = local.project
+#     team = "cloud"
+#     owner = "tugba"
+#     managed_by = "terraform"
+#   }
+# }
